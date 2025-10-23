042ddd3d9e09f608aec9828e46a6b95a603a6102
Turchikz <saitov.artur96@mail.ru>
2025-10-23 21:02:54 +0300

    add countdown and stopwatch

---
+++ saitov/countdown.py
@@ -0,0 +1,26 @@
+import time
+
+def countdown_timer(seconds):
+    """Таймер обратного отсчёта"""
+    while seconds:
+        # Преобразуем секунды в формат MM:SS
+        mins, secs = divmod(seconds, 60)
+        timer_format = f"{mins:02d}:{secs:02d}"
+        
+        # Выводим время с возвратом каретки (\r)
+        print(timer_format, end='\r')
+        
+        # Ждём 1 секунду
+        time.sleep(1)
+        
+        # Уменьшаем счётчик
+        seconds -= 1
+    
+    print("Время вышло! ⏰")
+
+# Получаем время от пользователя
+try:
+    total_seconds = int(input("Введите время в секундах: "))
+    countdown_timer(total_seconds)
+except ValueError:
+    print("Пожалуйста, введите целое число!")
